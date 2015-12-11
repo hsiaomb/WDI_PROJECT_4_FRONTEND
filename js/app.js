0420adf1f52796c88fda4db95c9a0f64b071ccdf
hsiaomb <hsiaomb@usc.edu>
2015-12-11 16:04:58 +0000

    MainRouter written in app.js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,16 @@ angular
   .constant('API', 'http://localhost:3000/api')
   .config(function($httpProvider) {
   	$httpProvider.interceptors.push('AuthInterceptor');
-  });
+  })
+  .config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function MainRouter($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "home.html"
+		});
+
+		$urlRouterProvider.otherwise('/');
+}
