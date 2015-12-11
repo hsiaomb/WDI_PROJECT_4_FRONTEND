7cb606d67d753ed8a7cc15cebb5a988648fed48a
hsiaomb <hsiaomb@usc.edu>
2015-12-11 15:45:01 +0000

    AuthInterceptor file changed.

---
+++ js/services/authInterceptor.js
@@ -0,0 +1,23 @@
+angular
+	.module('youtubeApp')
+	.factory('AuthInterceptor', AuthInterceptor);
+
+function AuthInterceptor(API, TokenService) {
+	return {
+
+		request: function(config) {
+			var token = TokenService.getToken();
+			if(config.url.match(API) && token) {
+				config.headers.Authorization = 'Bearer ' + token;
+			}
+			return config;
+		},
+
+		response: function(res) {
+			if(res.config.url.match(API) && res.data.token) {
+				TokenService.saveToken(res.data.token);
+			}
+			return res;
+		}
+	};
+}
