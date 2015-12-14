03ff027c7affe225f157182f8c5e4ff281770ca8
hsiaomb <hsiaomb@usc.edu>
2015-12-14 10:36:16 +0000

    Video Plays on multiple users. Working on Pause/Play functionality.

--- js/controllers/usersController.js
+++ js/controllers/usersController.js
@@ -21,6 +21,59 @@ function UsersController(User, TokenService, $scope) {
     $scope.$apply(self.messages.push(msg.text));
     console.log(self.messages.length);
   });
+  socket.on('playerState', function(state) {
+    YT.PlayerState = state;
+  });
+
+  // socket.on('video', function(vid) {
+  //   console.log("Video received");
+  //   $scope.$apply(self.messages.push(msg.text));
+  //   console.log(self.messages.length);
+  // });
+  function onPlayerStateChange(event) {
+      switch(event.data) {
+          case YT.PlayerState.ENDED:
+              console.log('Video has ended.');
+              break;
+          case YT.PlayerState.PLAYING:
+              console.log('Video is playing.');
+              break;
+          case YT.PlayerState.PAUSED:
+              var playerState = 2;
+              socket.emit('playerState', playerState);
+              break;
+          case YT.PlayerState.BUFFERING:
+              console.log('Video is buffering.');
+              break;
+          case YT.PlayerState.CUED:
+              console.log('Video is cued.');
+              break;
+      }
+  }
+  // -1 (unstarted)
+  // 0 (ended)
+  // 1 (playing)
+  // 2 (paused)
+  // 3 (buffering)
+  // 5 (video cued).
+  self.iframePlayer = function(vidId){
+    $('#container').html('<iframe id="player_'+vidId+'" width="420" height="315" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>');
+
+    new YT.Player('player_'+vidId, {
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+      });
+  };
+
+  socket.on('vidId', function(vid) {
+    self.iframePlayer(vid);
+  });
+
+  self.playVideo = function(vidId){
+    console.log('click');
+    socket.emit('vidId', vidId);
+  };
 
   function handleLogin(res) {
     var token = res.token ? res.token : null;
@@ -60,10 +113,6 @@ function UsersController(User, TokenService, $scope) {
 
   self.sendMessage = function() {
     socket.emit('message', self.message);
-  };
-
-  self.sendVideo = function(){
-    socket.emit('video', self.video);
   };
 
 
