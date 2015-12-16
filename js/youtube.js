1b9cdee5648d37cf58b70071e8ec66c2e389ea4f
hsiaomb <hsiaomb@usc.edu>
2015-12-16 10:32:20 +0000

    Initial heroku push

--- js/youtube.js
+++ js/youtube.js
@@ -1,43 +1,59 @@
-function log(msg) {
-    jQuery('#log').prepend(msg + '<br/>');
-}
+var apikey = 'AIzaSyDYwPzLevXauI-kTSVXTLroLyHEONuF9Rw';
 
-function onPlayerStateChange(event) {
-    switch(event.data) {
-        case YT.PlayerState.ENDED:
-            log('Video has ended.');
-            break;
-        case YT.PlayerState.PLAYING:
-            log('Video is playing.');
-            break;
-        case YT.PlayerState.PAUSED:
-            log('Video is paused.');
-            break;
-        case YT.PlayerState.BUFFERING:
-            log('Video is buffering.');
-            break;
-        case YT.PlayerState.CUED:
-            log('Video is cued.');
-            break;
-    }
-}
+$(function() {
+  var searchField = $('#search-input');
 
-function onPlayerReady(event) {
-  var embedCode = event.target.getVideoEmbedCode();
-  event.target.playVideo();
-}
+  $('#search-form').submit(function(e) {
+    e.preventDefault();
+  });
 
-$('Document').ready(function(){
-    $('.video-thumb').click(function(){
+  // /* Alert VideoID */
+  // $('#results').on('click','img[data-videoId]',function() {
+  //   alert($(this).attr('data-videoId'));
+  // });
+  // /* End Alert VideoID */
 
-        var vidId = $(this).attr('id');
-        $('#container').html('<iframe id="player_'+vidId+'" width="420" height="315" src="http://www.youtube.com/embed/' + vidId + '?enablejsapi=1&autoplay=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>');
-
-        new YT.Player('player_'+vidId, {
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-    });
 });
+
+function search() {
+  $('#results').html('');
+
+  q = $('#search-input').val();
+
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search", {
+      part: 'snippet, id',
+      q: q,
+      maxResults: 50,
+      type: 'video',
+      key: apikey
+    },
+    function(data) {
+      $.each(data.items, function(i, item) {
+        var output = getResults(item);
+
+        $('#results').append(output);
+      });
+    });
+  }
+
+  function getResults(item) {
+    var title = item.snippet.title;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoID = item.id.videoId;
+
+    var output = '<li>' +
+    '<div class="list-left">' +
+    '<img data-videoId="' + videoID + '" src="' + thumb + '">' +
+    '</div>' +
+    '<div class="list-right">' +
+    '<h3>' + title + '</h3>' +
+    '<p class="cTitle">' + channelTitle + '</p>' +
+    '</div>' +
+    '</li>' +
+    '<div class="clearfix"></div>' +
+    '';
+
+    return output;
+  }
